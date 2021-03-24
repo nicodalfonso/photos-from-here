ebf398013feeb51e2b72c03867f87cab999e47cb
Nico Alfonso <nicodalfonso@gmail.com>
2021-03-24 12:58:07 -0400

    Adds error handling for no location access.

--- src/components/photoFrame/PhotoFrame.tsx
+++ src/components/photoFrame/PhotoFrame.tsx
@@ -12,8 +12,8 @@ const positionOptions = {
 export const PhotoFrame = (): ReactElement => {
   const [photos, setPhotos] = useState<string[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string>("");
   const [photoIndex, setPhotoIndex] = useState<number>(0);
-  const [orientation, setOrientation] = useState<string>("");
 
   useEffect((): void => {
     const requestGeolocation = (): void => {
@@ -21,10 +21,6 @@ export const PhotoFrame = (): ReactElement => {
     };
     requestGeolocation();
   }, []);
-
-  useEffect(() => {
-    setOrientation(getOrientation(photos[photoIndex]));
-  }, [photos, photoIndex]);
 
   const getPhotos = async (pos: GeolocationPosition): Promise<void> => {
     const { coords }: { coords: GeolocationCoordinates } = pos;
@@ -36,21 +32,11 @@ export const PhotoFrame = (): ReactElement => {
   };
 
   const locationFailure = (e: unknown) => {
-    console.log(e);
+    setError("We were unable to find your location.");
   };
 
   const getImageUrls = (data: PhotoData[]): string[] => {
     return data.map((img) => `https://farm${img.farm}.staticflickr.com/${img.server}/${img.id}_${img.secret}.jpg`);
-  };
-
-  const getOrientation = (src: string): string => {
-    const img = new Image();
-    img.src = src;
-    if (img.naturalWidth > img.naturalHeight) {
-      return "w-full";
-    } else {
-      return "h-full";
-    }
   };
 
   const getPreviousPhoto = (): void => {
@@ -62,8 +48,11 @@ export const PhotoFrame = (): ReactElement => {
   };
 
   return (
+    // background
     <div className="flex justify-center items-center border-gray-500 border h-screen bg-indigo-100">
+      {/* left navigation pane */}
       <div className="bg-gray-700 h-2/5 w-32 rounded-l-lg flex items-center justify-center cursor-pointer" onClick={getPreviousPhoto}>
+        {/* left chevron */}
         <svg
           className="fill-current text-blue-200 h-24 w-24 cursor-pointer"
           xmlns="http://www.w3.org/2000/svg"
@@ -73,14 +62,15 @@ export const PhotoFrame = (): ReactElement => {
           <path d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm8-10a8 8 0 1 0-16 0 8 8 0 0 0 16 0zM7.46 9.3L11 5.75l1.41 1.41L9.6 10l2.82 2.83L11 14.24 6.76 10l.7-.7z" />
         </svg>
       </div>
+      {/* Spinner, current photo, or error message */}
       <div className="ring-4 w-1/2 rounded-t-lg flex justify-center items-center h-3/5 bg-gray-800 p-14 shadow-inner z-10">
-        {loading ? (
+        {error ? (
+          <p className="text-base text-indigo-100 text-center">{error}</p>
+        ) : loading ? (
           <Spinner />
         ) : photos ? (
-          <img className={`select-none ${orientation} shadow-2xl`} src={photos[photoIndex]} alt={photos[photoIndex]} />
-        ) : (
-          <p className="text-base text-black">"An Error Has Occurred"</p>
-        )}
+          <img className={`select-none shadow-2xl`} src={photos[photoIndex]} alt={photos[photoIndex]} />
+        ) : null}
       </div>
       <div className="bg-gray-700 h-2/5 w-32 rounded-r-lg flex items-center justify-center cursor-pointer" onClick={getNextPhoto}>
         <svg
