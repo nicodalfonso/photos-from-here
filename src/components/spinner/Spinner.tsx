c245bb041ecb5bfe67a1c3969e400bc2b589fbbc
Nico Alfonso <nicodalfonso@gmail.com>
2021-03-24 18:59:37 -0400

    tweaks spinner to use correct bg color

--- src/components/spinner/Spinner.tsx
+++ src/components/spinner/Spinner.tsx
@@ -3,5 +3,5 @@ import "./spinner.css";
 
 /** A simple loading spinner */
 export const Spinner = (): ReactElement => {
-  return <svg className="animate-spin h-24 w-24 border-8 border-l-0  border-white spinner rounded-full cursor-wait" viewBox="0 0 24 24" />;
+  return <svg className="animate-spin h-24 w-24 border-8 border-l-0  border-gray-800 spinner rounded-full cursor-wait" viewBox="0 0 24 24" />;
 };
